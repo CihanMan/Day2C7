f4a1ab78cae50b702bdb2987912ccd503fd81e72
CihanMan <114830325+CihanMan@users.noreply.github.com>
2022-10-12 01:18:53 -0400

    Update Ticket123.java

--- Ticket123.java
+++ Ticket123.java
@@ -1,17 +1,6 @@
 public class Ticket123 {
     public static void main(String[] args) {
-        System.out.println("Hello GitHub");
-        /*
-        some
-        codes
-        for
-        loginpage
-        */
-         /*
-        some
-        codes
-        for
-        landingpage
-        */
+        System.out.println("Resolve Conflict");
+      
     }
 }
